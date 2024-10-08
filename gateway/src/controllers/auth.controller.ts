1a4fddf608f08a5553b17079915fdcd434b814bf
nobodyknows1234 <88253500+nobodyknows1234@users.noreply.github.com>
2024-10-08 16:53:10 -0700

    endpoint verify

--- gateway/src/controllers/auth.controller.ts
+++ gateway/src/controllers/auth.controller.ts
@@ -5,20 +5,25 @@ import {
   HttpStatus,
   Inject,
   Post,
+  UseGuards,
 } from '@nestjs/common'
 import { CredentialsDto } from '../dto'
 import { firstValueFrom } from 'rxjs'
 import { ClientProxy } from '@nestjs/microservices'
 import { MessagesAuth } from '../types'
-import { IResponse, token } from '../interfaces'
+import { IResponse, token, User } from '../interfaces'
+import { AuthGuard } from 'src/guards/auth.guard'
+import { Authorization, GetUser } from 'src/decorators'
 
 @Controller('auth')
+@UseGuards(AuthGuard)
 export class AuthController {
   constructor(
     @Inject('CUSTOMER_SERVICE') private customerServiceClient: ClientProxy,
   ) {}
 
   @Post('login')
+  @Authorization(false)
   async logIn(@Body() loginDto: CredentialsDto): Promise<IResponse<token>> {
     const loginResponse: IResponse<token> = await firstValueFrom(
       this.customerServiceClient.send(MessagesAuth.LOGIN, loginDto),
@@ -28,5 +33,11 @@ export class AuthController {
       throw new BadRequestException(loginResponse.message)
 
     return loginResponse
+  }
+
+  @Post('verify')
+  @Authorization(true)
+  async verifyToken(@GetUser() user: User) {
+    return user
   }
 }
